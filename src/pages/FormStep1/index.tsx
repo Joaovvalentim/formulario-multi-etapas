0dfb516fe4f6aafb4e1d0331857906ff428a3522
joao <joao.victor.valentim@hotmail.com>
2022-09-12 14:25:04 -0300

    Adicionado UseNavigate

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,13 +1,33 @@
+import { useNavigate } from 'react-router-dom';
 import * as C from './styles'
 import { Theme } from '../../components/Theme'
 
+
 export const FormStep1 = () => {
+
+    const navigate = useNavigate();
+    const handleNextStep = () => {
+        navigate('/step2');
+    }
+
     return (
         <Theme>
             <C.Container>
-                Passo 1
-            </C.Container>
+                <p>Passo 1/3</p>
+                <h1>Nome</h1>
+                <p>Coloque seu nome</p>
 
+                <hr />
+
+                <label>
+                    Seu nome completo
+                    <input
+                        type="text"
+                        autoFocus
+                    />
+                </label>
+                <button onClick={handleNextStep}>Próximo</button>
+            </C.Container>
         </Theme>
 
 
