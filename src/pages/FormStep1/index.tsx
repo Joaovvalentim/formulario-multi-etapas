309288f5afdcd18ababb8312e81e1a165e9bafce
joao <joao.victor.valentim@hotmail.com>
2022-09-12 15:05:04 -0300

    Adicionado funcionalidades

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,19 +1,40 @@
 import { useNavigate } from 'react-router-dom';
 import * as C from './styles'
+import { useForm, FormActions } from '../../contexts/FormContext'
 import { Theme } from '../../components/Theme'
+import { ChangeEvent, useEffect } from 'react';
+import { info } from 'console';
 
 
 export const FormStep1 = () => {
 
     const navigate = useNavigate();
-    const handleNextStep = () => {
-        navigate('/step2');
-    }
+    const { state, dispatch } = useForm();// state tem os dados e em dispatch eu uso os dados, alterar ou executar açoes dentro do context usando o reducer
+    useEffect(() => {// este effect me diz em qual passo estou 
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        })
+    }, [])
 
+    const handleNextStep = () => {// este navigate usa o historioco para me jogar para proxima pagina ao clicar no botão
+       if(state.name!== ''){// verifica se o campo Nome esta vazio, caso nao ele deixa passar para a pagina 2
+        navigate('/step2');
+       }else{
+        alert("Digite seu nome")
+       }
+        
+    }
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {// este dispatch pega as informações do meu input em tempo real
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        })
+    }
     return (
         <Theme>
             <C.Container>
-                <p>Passo 1/3</p>
+                <p>Passo 1/3 - {state.currentStep}</p>
                 <h1>Nome</h1>
                 <p>Coloque seu nome</p>
 
@@ -24,6 +45,8 @@ export const FormStep1 = () => {
                     <input
                         type="text"
                         autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
                     />
                 </label>
                 <button onClick={handleNextStep}>Próximo</button>
